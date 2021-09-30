b963bd3dd878cf2cba3676ec7dd6a5b8598d133a
Andre Pina Dos Santos <5pinaa28@solent.ac.uk>
2021-09-30 15:47:53 +0100

    Updated code to display a longer multi-line message

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -1,3 +1,11 @@
 # Display message to the standart output
 print("System Failure Imminent!")
 print("System reboot has been initiated...")
+print()
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print()
+print()
+print("System online.")
