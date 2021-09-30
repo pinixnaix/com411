b55168d71f6720e8eb50033108e9bb24acf95c31
André Santos <5pinaa28@solent.ac.uk>
2021-09-30 21:25:37 +0100

    changes added to data_types

--- basics/input/data_types.py
+++ basics/input/data_types.py
@@ -1,7 +1,10 @@
 # Beep calculates BMI
+# Read in user data
 user_name = input("What is your name human?\n")
 user_age = int(input("\nHow old are you (in years)?\n"))
 user_heigth = float(input("\nHow tall are you (in meters)?\n"))
 user_weight = float(input("\nHow much do you weigh (in kilograms)?\n"))
+# Calculate the BMI
 user_bmi = user_weight/user_heigth**2
-print(f"{user_name} your are {user_age} and your bmi is {user_bmi:.2f}.")
+# Display result
+print(f"\n{user_name} your are {user_age} and your bmi is {user_bmi:.2f}.")
