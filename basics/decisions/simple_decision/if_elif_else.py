eb5f7b80869de7666c4673ba3efeeee916a01e2f
André Santos <5pinaa28@solent.ac.uk>
2021-10-07 08:48:36 +0100

    moved files to diferent folder

---
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -0,0 +1,12 @@
+print("Towards which direction should i paint (up, down, left or right)?")
+
+direction = input().lower()
+
+if direction == 'up':
+    print(f"\nI am painting in the {direction}ward direction!")
+elif direction == 'left':
+    print(f"\nI am painting in the {direction}ward direction!")
+elif direction == 'right':
+    print(f"\nI am painting in the {direction}ward direction!")
+else:
+    print("\nI am painting in the downward direction!")
