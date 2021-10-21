e971c1ac5d4180f312986e79b5a95066806d50dd
Andre Pina Dos Santos <5pinaa28@solent.ac.uk>
2021-10-21 16:32:39 +0100

    Added code to demonstrate a simple list.

---
+++ data/lists/simple_list.py
@@ -0,0 +1,16 @@
+def directions():
+    direct = []
+    direct.append("Move Forward")
+    direct.append("Move Backward")
+    direct.append("Move Left")
+    direct.append("Move Right")
+
+    return direct
+
+
+def run():
+    print(directions())
+
+
+if __name__ == "__main__":
+    run()
