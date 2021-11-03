bb6c1c929c49e65fc589f005704f5017a3659dc7
André Santos <5pinaa28@solent.ac.uk>
2021-11-03 12:20:34 +0000

    Added code to implement a text user interface.

--- data/olympics/tui.py
+++ data/olympics/tui.py
@@ -19,13 +19,28 @@ def menu():
        {"[team]":<10} Tally up medals for each team
        {"[exit]":<10} Exit the program
        """)
+    print("Your selection:")
+    return input()
 
 
-def run():
-    # started("hello")
-    # completed()
-    menu()
+def display_medal_tally(tally):
+    print(f"""
+    
+    | Gold      |{tally.get("Gold")}     |
+    | Silver    |{tally.get("Silver")}   |
+    | Bronze    |{tally.get("Bronze")}   | 
+       
+       """)
 
 
-if __name__ == '__main__':
-    run()
+def display_years(years):
+    for year in sorted(years, reverse=True):
+        print(year)
+
+
+def display_team_medal_tally(team_tally):
+    for team in team_tally:
+        print(team)
+        for key, values in team_tally.items():
+            print(f"\t{key}:{values} ", end='')
+        print()
