145cc08fab7269ded4e07ef759b0885f7743af6b
André Santos <5pinaa28@solent.ac.uk>
2021-12-13 15:19:06 +0000

    updated classes human and robot with methods to display objects

--- oop/human.py
+++ oop/human.py
@@ -10,6 +10,12 @@ class Human:
     def display(self):
         print(f"I am {self.name}")
 
+    def __repr__(self):
+        return f'Human(name={self.name}, age={self.age}, energy={self.energy})'
+
+    def __str__(self):
+        return f'Human {self.name} is {self.age} years old and has {self.energy}% energy!'
+
 
 if __name__ == "__main__":
     human = Human()
--- oop/robot.py
+++ oop/robot.py
@@ -9,6 +9,12 @@ class Robot:
     def display(self):
         print(f"I am {self.name}")
 
+    def __repr__(self):
+        return f'Robot(name={self.name}, age={self.age}, energy={self.energy})'
+
+    def __str__(self):
+        return f'Robot {self.name} is {self.age} years old and has {self.energy}% energy!'
+
 
 if __name__ == "__main__":
     robot = Robot()
